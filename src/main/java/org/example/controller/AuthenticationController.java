8ce6a15a52451af5e2f1250ca98941077d467f58
Ibrahim-abdulaev <97876746+Ibrahim-abdulaev@users.noreply.github.com>
2023-11-26 00:48:02 +0200

    add

--- src/main/java/org/example/controller/AuthenticationController.java
+++ src/main/java/org/example/controller/AuthenticationController.java
@@ -7,10 +7,7 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.HashMap;
 import java.util.Map;
@@ -24,20 +21,22 @@ public class AuthenticationController {
 
     @PostMapping("/login")
     @CrossOrigin
-    public ResponseEntity<?> createUser(@RequestBody User user) {
+    public @ResponseBody Map<String, Object> createUser(@RequestBody User user) {
         Map<String, Object> object = new HashMap<>();
         if (!userService.createUser(user)) {
             user.setMessage("Bad_request");
             user.setStatus(400);
             object.put("message", user.getMessage());
             object.put("status", user.getStatus());
-            return new ResponseEntity<>(object, HttpStatus.BAD_REQUEST);
+            return object;
         }
+        user.setUsername(user.getUsername());
+        user.setPassword(user.getPassword());
         user.setMessage("OK");
         user.setStatus(200);
         object.put("message", user.getMessage());
         object.put("status", user.getStatus());
-        return ResponseEntity.ok(object);
+        return object;
     }
 
 /*let response = await fetch('https://backend-web-service-ovks.onrender.com/login', {
