1d5b610f81be6476f8d50decc56b41b183943de2
Ibrahim-abdulaev <97876746+Ibrahim-abdulaev@users.noreply.github.com>
2023-11-26 00:40:19 +0200

    add

--- src/main/java/org/example/controller/AuthenticationController.java
+++ src/main/java/org/example/controller/AuthenticationController.java
@@ -37,6 +37,20 @@ public class AuthenticationController {
         user.setStatus(200);
         object.put("message", user.getMessage());
         object.put("status", user.getStatus());
-        return ResponseEntity.ok().body(object);
+        return ResponseEntity.ok(object);
     }
+
+/*let response = await fetch('https://backend-web-service-ovks.onrender.com/login', {
+            method: "POST", // *GET, POST, PUT, DELETE, etc.
+            mode: "cors", // no-cors, *cors, same-origin
+            cache: "no-cache", // *default, no-cache, reload, force-cache, only-if-cached
+            credentials: "same-origin", // include, *same-origin, omit
+            headers: {
+                'Content-Type': 'application/json;charset=utf-8'
+            },
+            body: JSON.stringify({
+                username: login,
+                password: password
+            })
+        })*/
 }
